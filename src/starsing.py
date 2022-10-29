d25c601a3d74f26f23d5d316c055a6e2f0fe6b73
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-29 19:09:18 -0400

    Update starsing.py

--- src/starsing.py
+++ src/starsing.py
@@ -5,15 +5,52 @@ def find_index_of_1st(char: chr, string: str) -> int:
         if string[index] == char:
             return index
 
+        
 def is_element_at(index: int, char: chr, string: str) -> bool:
     return (string[index] == char)
+
 
 def count_occurence(char: chr, string: str) -> int:
     return sum((char == original ) for original in string)
 
+
 def contains(char: chr, string: str) -> bool:
     return (char in string)
+
 
 def contains(substring: str, string: str) -> bool:
     return (substring in string)
 
+
+def is_homogenous(string: str) -> bool:
+    for index in range(len(string)):
+        if string[i] == [i + 1]:
+            continue;
+        else:
+            return False
+    return True
+
+
+def is_only_alpha(string: str) -> bool:
+    for char in string:
+        if char.isalpha():
+            continue
+        else:
+            return False
+    return True
+
+
+def is_only_numeric(string: str) -> bool:
+    for char in string:
+        if char.isdigit():
+            continue
+        else:
+            return False
+    return True
+
+
+def is_alphanumeric(string: str) -> bool:
+    for char in string:
+        if char.isdigit() and char.isalpha():
+            continue
+        else:
